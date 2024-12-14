87a28a6df02110745ba1a245fac4df74bc622ab3
grapiayang <enterpia@naver.com>
2024-12-14 20:07:26 +0900

    2Days

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>고객 관리 시스템</h1>
+      <Customer />
     </div>
   );
 }
